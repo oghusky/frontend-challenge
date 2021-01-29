3420e187fb9e5a7c54154dd582609cfdfc490018
oghusky <philiposimmons@gmail.com>
2021-01-29 17:57:33 -0500

    Subject: Shuffle tiles
    Body: Added script to shuffle tiles on init

--- puzzle/assets/js/app.js
+++ puzzle/assets/js/app.js
@@ -9,15 +9,24 @@ window.addEventListener("load", () => {
       const board = document.createElement("ul");
       board.classList.add("new-puzzle")
       board.style.width = `300px`;
-      for (var i = 0; i < this.tiles - 1; i++) {
+      this.shuffle().forEach(tile => {
         const token = document.createElement("li")
         token.classList.add("tile");
-        token.innerHTML = `${i + 1}`
+        token.innerHTML = `${tile}`
         board.appendChild(token)
-      }
+      })
       figure.replaceChild(board, oldBoard);
+    }
+    shuffle() {
+      const tilesArr = []
+      for (var i = 0; i < this.tiles - 1; i++) {
+        tilesArr.push(i + 1);
+      }
+      const randomizeTiles = tilesArr.sort(() => Math.random() - 0.5)
+      return randomizeTiles
     }
   }
   const puzzle = new Puzzle(9);
   puzzle.init();
+  // puzzle.shuffle();
 })
