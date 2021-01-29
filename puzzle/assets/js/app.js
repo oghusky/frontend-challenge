b3569f1923ba8243bba0763d7fe16552348d83d7
oghusky <philiposimmons@gmail.com>
2021-01-29 13:06:09 -0500

    Subject: Create board class
    Body: created game board class using js. able to get game to resize depending on number of tiles in argument

--- puzzle/assets/js/app.js
+++ puzzle/assets/js/app.js
@@ -1,1 +1,23 @@
-alert("hello")
+window.addEventListener("load", () => {
+  class Puzzle {
+    constructor(tiles) {
+      this.tiles = tiles;
+    }
+    init() {
+      const figure = document.querySelector("figure.puzzle-figure");
+      const oldBoard = document.querySelector("ul.puzzle");
+      const board = document.createElement("ul");
+      board.classList.add("new-puzzle")
+      board.style.width = `300px`;
+      for (var i = 0; i < this.tiles - 1; i++) {
+        const token = document.createElement("li")
+        token.classList.add("tile");
+        token.innerHTML = `${i + 1}`
+        board.appendChild(token)
+      }
+      figure.replaceChild(board, oldBoard);
+    }
+  }
+  const puzzle = new Puzzle(9);
+  puzzle.init();
+})
