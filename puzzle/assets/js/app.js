e68722bc48465a39de6407c52ff360cc3a0f0c09
oghusky <philiposimmons@gmail.com>
2021-01-30 03:48:53 -0500

    Subject: Code Clean Up
    Body: Dried up tileactions and removed unnecessary ls item setting

--- puzzle/assets/js/app.js
+++ puzzle/assets/js/app.js
@@ -47,64 +47,46 @@ window.addEventListener("load", () => {
       for (var i = 0; i < this.tiles - 1; i++) {
         tilesArr.push(i + 1);
       }
+      // randomizes array of given tile number
       return tilesArr.sort(() => .5 - Math.random());
     }
     setEmptyPosition() {
+      // same block is set as empty for each game
       localStorage.setItem("emptyPosition", JSON.stringify([200, 200]))
-      localStorage.setItem("notEmptyPosition", JSON.stringify([
-        [0, 0], [100, 0], [200, 0],
-        [0, 100], [100, 100], [200, 100],
-        [0, 200], [100, 200]]))
     }
     checkPosition() {
       document.querySelectorAll(".tile").forEach(tile => {
-        // on click loop thru all unavailable positions
-        // if position is not unavailable pass
-        // if position is available replace localStorage item with current positon
-        // and move tile to newly available position then replace localstorage with old position
         tile.addEventListener("click", (e) => {
           // check for position of tile compared to other tiles
           const empty = JSON.parse(localStorage.getItem("emptyPosition"));
+          // if position is available replace localStorage item with current positon
+          // and move tile to newly available position then replace localstorage with old position
           // move right
           if ((parseInt(e.target.style.left.split("px")[0]) + 100 === empty[0]) && (parseInt(e.target.style.top.split("px")[0]) === empty[1])) {
-            // set current tile position to empty position from ls
-            // set empty position in ls to tiles old position
-            localStorage.setItem("tempEmptyPosition", JSON.stringify([parseInt(e.target.style.left.split("px")[0]), parseInt(e.target.style.top.split("px")[0])]))
-            e.target.style.left = `${empty[0]}px`;
-            e.target.style.top = `${empty[1]}px`;
-            const temp = JSON.parse(localStorage.getItem("tempEmptyPosition"))
-            localStorage.setItem("emptyPosition", JSON.stringify(temp))
+            this.tileAction(e, empty);
           }
           // move left
           if ((parseInt(e.target.style.left.split("px")[0]) - 100 === empty[0]) && (parseInt(e.target.style.top.split("px")[0]) === empty[1])) {
-            // set current tile position to empty position from ls
-            // set empty position in ls to tiles old position
-            localStorage.setItem("tempEmptyPosition", JSON.stringify([parseInt(e.target.style.left.split("px")[0]), parseInt(e.target.style.top.split("px")[0])]))
-            e.target.style.left = `${empty[0]}px`;
-            e.target.style.top = `${empty[1]}px`;
-            const temp = JSON.parse(localStorage.getItem("tempEmptyPosition"))
-            localStorage.setItem("emptyPosition", JSON.stringify(temp))
+            this.tileAction(e, empty);
           }
           // move down
           if ((parseInt(e.target.style.left.split("px")[0]) === empty[0]) && (parseInt(e.target.style.top.split("px")[0]) + 100 === empty[1])) {
-            console.log("move down")
-            localStorage.setItem("tempEmptyPosition", JSON.stringify([parseInt(e.target.style.left.split("px")[0]), parseInt(e.target.style.top.split("px")[0])]))
-            e.target.style.left = `${empty[0]}px`;
-            e.target.style.top = `${empty[1]}px`;
-            const temp = JSON.parse(localStorage.getItem("tempEmptyPosition"))
-            localStorage.setItem("emptyPosition", JSON.stringify(temp))
           }
           // move up
           if ((parseInt(e.target.style.left.split("px")[0]) === empty[0]) && (parseInt(e.target.style.top.split("px")[0]) - 100 === empty[1])) {
-            console.log("move down")
-            localStorage.setItem("tempEmptyPosition", JSON.stringify([parseInt(e.target.style.left.split("px")[0]), parseInt(e.target.style.top.split("px")[0])]))
-            e.target.style.left = `${empty[0]}px`;
-            e.target.style.top = `${empty[1]}px`;
-            const temp = JSON.parse(localStorage.getItem("tempEmptyPosition"))
-            localStorage.setItem("emptyPosition", JSON.stringify(temp))
+            this.tileAction(e, empty);
           }
         })
       })
+    }
+    tileAction(e, empty) {
+      // set current tile position to empty position from ls
+      // set empty position in ls to tiles old position
+      localStorage.setItem("tempEmptyPosition", JSON.stringify([parseInt(e.target.style.left.split("px")[0]), parseInt(e.target.style.top.split("px")[0])]))
+      e.target.style.left = `${empty[0]}px`;
+      e.target.style.top = `${empty[1]}px`;
+      const temp = JSON.parse(localStorage.getItem("tempEmptyPosition"))
+      localStorage.setItem("emptyPosition", JSON.stringify(temp))
     }
   }
   // create new puzzle
